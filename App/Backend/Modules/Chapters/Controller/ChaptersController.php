f094f67686db665cd6404b9eb95a1e11d3eb6332
Romain CHARLES <romain.charles@rocketmail.com>
2020-02-25 17:37:43 +0100

    Début de la mise en place du module de création de chapitres

---
+++ App/Backend/Modules/Chapters/Controller/ChaptersController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Backend\Modules\Chapters\Controller;
+
+
+
+class ChaptersController extends BackController
+{
+    public function saveChapter ()
+    {
+        
+    }
+}
---
+++ App/Backend/Modules/Chapters/Views/addchapters.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+    <title>Blog Auteur</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+<h2>Ajout d'un chapitre</h2>
+
+<h3>Le titre du chapitre se trouvera ici</h3>
+
+<p>Le texte du chapitre se trouvera là</p>
+
+<button type="submit">Enregistrer le chapitre</button>
+</body>
+</html>
+
+<!-- route : http://blogauteur.romaincharlesdemonstrator.ovh/admin/addchapters
+ <route url="/admin/addchapters" module="Chapters" action="addchapters"/>-->
