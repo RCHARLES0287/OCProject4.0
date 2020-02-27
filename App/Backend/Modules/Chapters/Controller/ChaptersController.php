48b85dcb19b9690e827c6f348bfc27b590e84b4e
Romain CHARLES <romain.charles@rocketmail.com>
2020-02-27 11:28:32 +0100

    Changement nommage méthodes de ChaptersController

--- App/Backend/Modules/Chapters/Controller/ChaptersController.php
+++ App/Backend/Modules/Chapters/Controller/ChaptersController.php
@@ -5,7 +5,7 @@ namespace App\Backend\Modules\Chapters\Controller;
 
 class ChaptersController extends BackController
 {
-    public function saveChapter ()
+    public function executeSaveChapter ()
     {
 
     }
