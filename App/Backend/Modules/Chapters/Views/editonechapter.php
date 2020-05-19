161e16d4e0d9724e5a57f0c7c2e08c08bd6255aa
Romain CHARLES <romain.charles@rocketmail.com>
2020-05-19 14:50:37 +0200

    Suppression plugins TinyMCE inutiles et bloquants

--- App/Backend/Modules/Chapters/Views/editonechapter.php
+++ App/Backend/Modules/Chapters/Views/editonechapter.php
@@ -23,8 +23,8 @@
     <script>
         tinymce.init({
             selector: 'textarea',
-            plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-            toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+            // plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+            toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent',
             toolbar_mode: 'floating',
             tinycomments_mode: 'embedded',
             tinycomments_author: 'Author name',
