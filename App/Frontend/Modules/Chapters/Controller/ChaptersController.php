ebba4ef132bc013f07fa7ced3e0a60b8fa11478c
Romain CHARLES <romain.charles@rocketmail.com>
2020-05-26 18:00:48 +0200

    Création des fichiers pour affichage chapitres côté frontend

---
+++ App/Frontend/Modules/Chapters/Controller/ChaptersController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Backend\Modules\Chapters\Controller;
+
+use Entity\ChapterEntity;
+use Model\ChaptersManager;
+use Exception;
+use OCFram\BackController;
+use OCFram\Entity;
+use OCFram\HTTPRequest;
+
+
+class ChaptersController extends BackController
+{
+    public function executeShowallchapters (HTTPRequest $request)
+    {
+        $chaptersManager = new ChaptersManager();
+
+        $allChaptersData = $chaptersManager->getAllChapters();
+
+        $this->page->addVar('chapters', $allChaptersData);
+
+//        var_dump($allChaptersData);
+    }
+
+
+    public function executeCommentonechapter (HTTPRequest $request)
+    {
+
+    }
+
+
+}
---
+++ App/Frontend/Modules/Chapters/Views/showallchapters.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+    <title>Blog Auteur</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+<h2>Liste de tous les chapitres</h2>
+
+<h3>Il y aura les titres de chapitres :</h3>
+
+<h3>Il y aura aussi le texte des chapitres</h3>
+
+
+<ul>
+
+    <?php
+
+    /** @var \Entity\ChapterEntity $chapter */
+    foreach ($chapters as $chapter)
+    {
+        ?>
+        <li>
+            Chapitre <?= $chapter->chapter_number() ?>: <?= $chapter->title() ?>.<br/>
+            <?= $chapter->text() ?>
+            <!--        Titre du chapitre : --><?//= $chapter->title() ?><!--<br/>-->
+
+            <?php echo $chapter->id() ?>
+            <form method="post" action="/visitor/commentonechapter">
+                <input id="chapter_id" type="hidden" name="chap_id" value='<?= $chapter->id() ?>'/><br />
+                <input name="comment_chapter_button" type="submit" value="Commenter">
+            </form>
+
+        </li>
+
+        <?php
+    }
+    ?>
+
+</ul>
+
+
+</body>
+</html>
+
+
+
