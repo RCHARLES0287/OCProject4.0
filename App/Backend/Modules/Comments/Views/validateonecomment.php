c704d9f65e0c4dc9dda48a6c1700dcd8b4f83364
Romain CHARLES <romain.charles@rocketmail.com>
2020-07-24 17:38:06 +0200

    Passage de la validation de commentaire de POST à GET

--- App/Backend/Modules/Comments/Views/validateonecomment.php
+++ App/Backend/Modules/Comments/Views/validateonecomment.php
@@ -18,7 +18,7 @@
 
     <p class="message-au-visiteur">Le commentaire a bien été validé.</p>
 
-    <form method="post" action="/admin/showonechapter">
+    <form method="get" action="/admin/showonechapter">
         <input id="chapter_id" type="hidden" name="chap_id" value='<?= $chapterId ?>'/><br />
         <input name="show_chapter_button" type="submit" value="Retour au chapitre">
     </form>
